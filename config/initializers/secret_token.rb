f0292c6429f1059dfb14e390556b2e3519e765b5
syunki <info@syunki.me>
2014-02-14 16:31:58 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AntennaSite::Application.config.secret_key_base = '32603a0f6b82c653fdf4a91e0fccd6c1e464621e3453eae85b34f73016c322f6187145576ad30739b1bd0650693ad25888e60e4c5d8f43b8ca16ec4201b09fe5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AntennaSite::Application.config.session_store :cookie_store, key: '_antenna-site_session'
